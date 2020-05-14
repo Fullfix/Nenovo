8270e041380be4799aa6b476a57ef6d408598577
Nikita <nbreskanu73@gmail.com>
2020-05-14 12:37:57 +0400

    added token validation

--- server.js
+++ server.js
@@ -1,15 +1,95 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const User = require('./models/User')
+const bodyParser = require('body-parser')
+const {isNewSessionRequired, isAuthRequired, verifyToken} = require('./services/authUtils')
 
 require('dotenv/config')
-require('./models/User')
 require('./models/Article')
 require('./models/Category')
 
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.user(async (req, res, next) => {
+    let apiUrl = req.originalUrl
+    let httpMethod = req.method
+    req.session = {}
+
+    if (isNewSessionRequired(httpMethod, apiUrl)) {
+        req.newSessionRequired = true
+    }
+    else if (isAuthRequired(httpMethod, apiUrl)) {
+        let authHeader = req.header('Authorization')
+        let sessionID = authHeader.split(' ')[1];
+        if (sessionID) {
+            userData = verifyToken(sessionID)
+            if (userData) {
+                req.session.userData = userData
+                req.session.sessionID = sessionID
+            }
+            else {
+                return res.status(401).send({
+                    ok: false,
+                    error: {
+                        reason: "Invalid SessionToken",
+                        code: 401
+                    }
+                })
+            }
+        }
+        else {
+            return res.status(401).send({
+                ok: false,
+                error: {
+                    reason: "Missing SessionToken",
+                    code: 401
+                }
+            })
+        }
+    }
+    next()
+})
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(async (req, res, next) => {
+    if (!res.data) {
+        return res.status(404).send({
+            status: false,
+            error: {
+                reason: "Invalid Endpoint",
+                code: 404
+            }
+        })
+    }
+    if (req.newSessionRequired && req.session.userData) {
+        let user = await User.findById(req.session.userData.id)
+        try {
+            let jwt = await user.generateJWT()
+            res.setHeader('session-token', jwt)
+            res.data['session-token'] = jwt
+        }
+        catch (e) {
+            console.log(`e: ${e}`)
+        }
+    }
+    if (req.session && req.session.sessionID) {
+        try {
+            res.setHeader('session-token', req.session.sessionID)
+            res.data['session-token'] = req.session.sessionID
+        }
+        catch (e) {
+            console.log(`e: ${e}`)
+        }
+    }
+    res.status(res.statusCode || 200)
+    .send({status: true, response: res.data})
+})
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
 mongoose.connect(
     process.env.DB_CONNECTION,
---
+++ services/authUtils.js
@@ -0,0 +1,41 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+require('dotenv/config')
+const secret = process.env.SECRET_KEY
+const newSessionRoutes = [
+    {path: '/api/user/authenticate', method: 'POST'},
+]
+const authRoutes = [
+    {path: '/api/article/recommended', method: 'GET'},
+    {path: '/api/article/watch', method: 'POST'},
+    {path: '/api/user/changekeywords', method: 'PUT'}
+]
+let exports = module.exports = {}
+
+exports.isNewSessionRequired = (httpMethod, url) => {
+    for (let routeObj of newSessionRoutes) {
+        if (httpMethod === routeObj.method && url === routeObj.path) {
+            return true
+        }
+    }
+    return false
+}
+
+exports.isAuthRequired = (httpMethod, url) => {
+    for (let routeObj of authRoutes) {
+        if (httpMethod === routeObj.method && url === routeObj.path) {
+            return true
+        }
+    }
+    return false
+}
+
+exports.verifyToken = (jwtToken) => {
+    try {
+        jwt.verify(jwtToken, secret)
+    }
+    catch(e) {
+        console.log(`e: ${e}`)
+        return null
+    }
+}
