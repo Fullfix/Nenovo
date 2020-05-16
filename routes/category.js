b41033b2e57feec2204860006d8dc38d33d2193a
Nikita <nbreskanu73@gmail.com>
2020-05-16 15:47:22 +0400

    added category/articles API

--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const ObjectId = require('mongoose').Types.ObjectId
 const Category = require('../models/Category')
+const Article = require('../models/Article')
 const { translateCategories } = require('../data')
 
 router.get('/names', async (req, res, next) => {
@@ -9,6 +11,40 @@ router.get('/names', async (req, res, next) => {
     console.log(names)
     res.data = {
         names: names
+    }
+    next()
+})
+
+router.get('/articles', async (req, res, next) => {
+    let categoryName = req.body.name
+    if (!categoryName) {
+        return res.status(400).send({
+            ok: false,
+            error: {
+                reason: 'Missing parameter: name',
+                code: 400
+            }
+        })
+    }
+    let category = await Category.findOne({name: categoryName}).exec()
+    if (!category) {
+        return res.status(400).send({
+            ok: false,
+            error: {
+                reason: 'Category with this name does not exist',
+                code: 400
+            }
+        })
+    }
+    let date = new Date()
+    date.setDate(date.getDay() - 3)
+    let articles = await Article.find({
+        '_id': { $in: category.articles},
+        'date': { $gte: date }
+    }).sort('-date').exec()
+    let articlesObj = articles.map(article => article.toObject())
+    res.data = {
+        articles: articlesObj
     }
     next()
 })
