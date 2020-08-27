b5075ee0a71d009c5e24877560c8d1738e58e4ea
Nikita Breskanu <50447080+Fullfix@users.noreply.github.com>
2020-08-27 11:14:51 +0400

    frontend: added article helper

---
+++ frontend/src/helpers/article.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+export const getRecent = async () => {
+    const res = await axios.get('/api/article/recent');
+    return res.data.response;
+}
+
+export const getRecommended = async () => {
+    let res;
+    try {
+        res = (await axios.get('/api/article/recommended')).data;
+    } catch (err) {
+        res = err.response.data;
+    }
+    if (res.ok) {
+        return { success: true, articles: res.response };
+    }
+    return { success: false, error: res.error.reason };
+}
+
+export const watchArticle = async (id) => {
+    let res;
+    try {
+        res = (await axios.post('/api/article/watch'), { id }).data;
+    } catch (err) {
+        res = err.response.data;
+    }
+    if (res.ok) {
+        return { success: true, articles: res.response };
+    }
+    return { success: false, error: res.error.reason };
+}
