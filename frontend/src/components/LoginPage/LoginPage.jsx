2795e39208f1a1ce00dea695bd8aea57b62dc395
Nikita Breskanu <50447080+Fullfix@users.noreply.github.com>
2020-09-05 17:02:16 +0400

    login page responsive for mobile

--- frontend/src/components/LoginPage/LoginPage.jsx
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -23,7 +23,7 @@ const LoginPage = () => {
                 <Link className="left-title" to='/'><h1 >НеНово</h1></Link>
                 <div className="left-description">
                     <p>
-                        <span style={{fontSize: 36 + 'px'}}>Входя или <span style={{color: 'white'}}>регистрируясь</span> на сайт, </span>
+                        <span style={{fontSize: '2em'}}>Входя или <span style={{color: 'white'}}>регистрируясь</span> на сайт, </span>
                         вы получаете возможность видеть <span style={{color: 'white'}}>рекомендации,</span> а также настраивать <span style={{color: 'white'}}>ключевые слова </span>
                         по интересам 
                     </p>
